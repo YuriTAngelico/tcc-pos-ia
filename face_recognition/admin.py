43e7281b422691a1f170e35672a20fbe51f8f0c2
Yuri Trevisan Angelico <yuri.t.angelico@hotmail.com>
2021-07-25 20:32:31 -0300

    first commit of this work for the conclusion of my post graduation in AI

--- face_recognition/admin.py
+++ face_recognition/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
-
+from simple_history.admin import SimpleHistoryAdmin
+from .models import *
 # Register your models here.
+
+class EmployeeHistoryAdmin(SimpleHistoryAdmin):
+    list_display = ['first_name', 'last_name', 'age', 'job_position', 'timestamp',]
+    history_list_display = ['first_name', 'last_name', 'age', 'job_position', 'timestamp',]
+    list_filter = ['age', 'job_position',]
+admin.site.register(Employee, EmployeeHistoryAdmin)
+
+
+class PhotoHistoryAdmin(SimpleHistoryAdmin):
+    list_display = ['employee', 'mask', 'timestamp',]
+    history_list_display = ['employee', 'mask', 'timestamp',]
+    list_filter = ['employee', 'mask', 'timestamp',]
+admin.site.register(Photo, PhotoHistoryAdmin)
+
+
+class AIFaceDetectorHistoryAdmin(SimpleHistoryAdmin):
+    list_display = ['name', 'timestamp',]
+    history_list_display = ['name', 'timestamp',]
+    list_filter = ['name', 'timestamp',]
+admin.site.register(AIFaceDetector, AIFaceDetectorHistoryAdmin)
+
+
+class AIRecognizerHistoryAdmin(SimpleHistoryAdmin):
+    list_display = ['name', 'timestamp',]
+    history_list_display = ['name', 'timestamp',]
+    list_filter = ['name', 'timestamp',]    
+admin.site.register(AIRecognizer, AIRecognizerHistoryAdmin)
+
+
+class AILabelHistoryAdmin(SimpleHistoryAdmin):
+    list_display = ['name', 'timestamp',]
+    history_list_display = ['name', 'timestamp',]
+    list_filter = ['name', 'timestamp',]
+admin.site.register(AILabel, AILabelHistoryAdmin)
--- face_recognition/models.py
+++ face_recognition/models.py
@@ -3,17 +3,12 @@ from simple_history.models import HistoricalRecords
 from django.conf import settings
 import os
 
-def models_path():
-    return os.path.join(settings.LOCAL_FILE_DIR, 'models')
-def ia_files_path():
-    return os.path.join(settings.LOCAL_FILE_DIR, 'ia_files')
-
 # Create your models here.
 
 class Employee(models.Model):
     first_name = models.CharField(max_length=30, default='first_name')
     last_name = models.CharField(max_length=30, default='last_name')
-    age = models.IntegerField(max_length=3, default=0)
+    age = models.IntegerField(default=0)
     job_position = models.CharField(max_length=30, default='*')
     timestamp = models.DateTimeField(auto_now=True)
     history = HistoricalRecords()
@@ -21,8 +16,9 @@ class Employee(models.Model):
     def __str__(self):
         return self.first_name
 
-class Photos(models.Model):
+class Photo(models.Model):
     employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    photo = models.FileField(upload_to='employee_photo/')
     bloob = models.JSONField(default=dict)
     mask = models.BooleanField(default=False)
     timestamp = models.DateTimeField(auto_now=True)
@@ -31,11 +27,35 @@ class Photos(models.Model):
     def __str__(self):
         return self.employee.first_name
 
-class IAModel(models.Model):
-    model = models.FileField(upload_to='models')
-    model_path = models.FilePathField(path=models_path, recursive=True)
+class AIModel(models.Model):
+    name = models.CharField(max_length=30, default='my_model')
+    model = models.FileField(upload_to='models/')
+    timestamp = models.DateTimeField(auto_now=True)
+    history = HistoricalRecords()
 
-class IAFiles(models.Model):
-    face_detection_model = models.FilePathField(path=ia_files_path, recursive=True)
-    recognizer = models.FilePathField(path=ia_files_path, recursive=True)
-    labels = models.FilePathField(path=ia_files_path, recursive=True)
+class AIFaceDetector(models.Model):
+    name = models.CharField(max_length=30, default='face_detector')
+    face_detection_model = models.FileField(upload_to='ai_face_detector/')
+    timestamp = models.DateTimeField(auto_now=True)
+    history = HistoricalRecords()
+
+    def __str__(self):
+        return self.name
+
+class AIRecognizer(models.Model):
+    name = models.CharField(max_length=30, default='recognizer')
+    recognizer = models.FileField(upload_to='ai_recognizer/')
+    timestamp = models.DateTimeField(auto_now=True)
+    history = HistoricalRecords()
+
+    def __str__(self):
+        return self.name
+
+class AILabel(models.Model):
+    name = models.CharField(max_length=30, default='labels')
+    labels = models.FileField(upload_to='ai_labels/')
+    timestamp = models.DateTimeField(auto_now=True)
+    history = HistoricalRecords()
+
+    def __str__(self):
+        return self.name
